cc966de519995f7ac5e3c8f0a8c1e8e62ab81bbc
Tiejli Li <tiejli@yahoo.com>
2016-06-25 16:11:43 -0700

    update packages.

--- provisoner.sh
+++ provisoner.sh
@@ -8,3 +8,14 @@ sudo apt-get update
 
 sudo apt-get install -y git vim keychain
 
+sudo apt-get install -y screen
+sudo apt-get install -y firefox cloc curl subversion
+sudo apt-get install -y python2.7-mysqldb python-dev
+ 
+sudo apt-get install -y build-essential chrpath libssl-dev libxft-dev
+sudo apt-get install -y libfreetype6 libfreetype6-dev libfontconfig1 
+sudo apt-get install -y libfontconfig1-dev
+sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+sudo apt-get install -y mysql-server
+mysqladmin -u root password password
