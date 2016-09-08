e2538e0bef9d28d6cbb8a90dcd222aa883727279
tieli <tiejli@yahoo.com>
2016-09-07 23:57:56 -0700

    script used to bootstrap a vagrant node.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,14 @@
 #!/usr/bin/env bash
 
-###Install Vundle
-#mkdir -p .vim/bundle
-#cd ~/.vim/bundle && git clone https://github.com/VundleVim/Vundle.vim.git
-#
-###Install Pathogen
-##mkdir -p ~/.vim/autoload && \
-##curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-#
-##cd ~/.vim/bundle && git clone git://github.com/godlygeek/tabular.git
-#
-#ln -s /vagrant/works ~/works
-#
-### Go back to home directory.
-#cd ~
-#
-### Install Python related stuff
-#wget https://bootstrap.pypa.io/get-pip.py
-#
-#sudo python ~/get-pip.py
-#sudo pip install virtualenv
-#sudo pip install ansible
-#
-### Fetch all rc files
-##rc_files=(.railsrc .bashrc .gemrc .irbrc .ansible.cfg .bash_aliases .screenrc .gemrc .vimrc)
-#
-#root_url="https://raw.githubusercontent.com/tieli/dotfiles/master"
-#
-##for rc_file in ${rc_files[@]}
-##do
-##    echo "${root_url}/$rc_file"
-##    wget -N -P $HOME "${root_url}/$rc_file"
-##done
-#
-#cd ~ && wget -N -P $HOME "${root_url}/.vimrc"
+## Fetch all rc files
+rc_files=(.railsrc .bashrc .gemrc .irbrc .ansible.cfg .bash_aliases .screenrc .gemrc .vimrc)
 
-curl -L https://www.opscode.com/chef/install.sh | bash
+root_url="https://raw.githubusercontent.com/tieli/dotfiles/master"
+
+for rc_file in ${rc_files[@]}
+do
+    echo "${root_url}/$rc_file"
+    wget -N -P $HOME "${root_url}/$rc_file"
+done
+
 
