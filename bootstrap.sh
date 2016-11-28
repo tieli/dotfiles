9f9172cd9f85a8b2088041238235b1156b6ab478
tieli <tiejli@yahoo.com>
2016-11-28 23:35:19 +0000

    add a python script to configure rsa key

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,26 +11,3 @@ do
     wget -N -P $HOME "${root_url}/$rc_file"
 done
 
-sudo apt-get update
-sudo apt-get install -y git vim build-essential libssl-dev libffi-dev python-dev
-
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-#mkdir -p ~/.vim/autoload && \
-#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-vim_rc_url="https://raw.githubusercontent.com/tieli/dotfiles/master/.vimrc"
-wget -N -P $HOME $vim_rc_url
-
-vim +PluginInstall +qall
-
-ln -s /vagrant/works ~/works
-
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python ~/get-pip.py
-sudo pip install virtualenv
-sudo pip install ansible
-
-
-
