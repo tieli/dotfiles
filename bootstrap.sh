00a9dfcb09b431581448d2cad162aaea75e2aee8
tieli <tiejli@yahoo.com>
2016-12-08 08:41:19 +0000

    add script for installing PhantomJS link .gitconfig to HOME directory.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,13 @@ do
     wget -N -P $HOME "${root_url}/$rc_file"
 done
 
+vim_rc_url="https://raw.githubusercontent.com/tieli/dotfiles/master/.vimrc"
+wget -N -P $HOME $vim_rc_url
+
+vim +PluginInstall +qall
+
+ln -s /vagrant/works ~/works
+
 sudo apt-get update
 sudo apt-get install -y git vim build-essential libssl-dev libffi-dev python-dev
 
@@ -19,13 +26,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #mkdir -p ~/.vim/autoload && \
 #curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-vim_rc_url="https://raw.githubusercontent.com/tieli/dotfiles/master/.vimrc"
-wget -N -P $HOME $vim_rc_url
-
-vim +PluginInstall +qall
-
-ln -s /vagrant/works ~/works
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python ~/get-pip.py
@@ -38,3 +38,24 @@ ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
 ssh-keygen -f $HOME/.ssh/id_dsa -t dsa -N ''
 
 
+# Install PhantomJS
+
+PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+else
+	apt-get update
+	apt-get install -y build-essential chrpath libssl-dev libxft-dev 
+	apt-get install -y libfreetype6 libfreetype6-dev
+	apt-get install -y libfontconfig1 libfontconfig1-dev
+	cd ~
+	wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+	mv $PHANTOM_JS.tar.bz2 /usr/local/share/
+	cd /usr/local/share/
+	tar xvjf $PHANTOM_JS.tar.bz2
+	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
+	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
+	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
+	rm -fr $PHANTOM_JS.tar.bz2
+fi
