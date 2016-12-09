517867f5f0156c1b03749c354cb4a7f6a7288964
tieli <tiejli@yahoo.com>
2016-12-09 17:32:54 +0000

    check if file exists in user's home directory.

--- diff_all.sh
+++ diff_all.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+#todo
+# 1. get the list of dotfiles automatically
+
 rc_files=(.ansible.cfg .bash_aliases .bashrc .gemrc .gitconfig .irbrc .railsrc .screenrc .vimrc)
 
 for rc_file in ${rc_files[@]}
 do
-    diff $rc_file $HOME/$rc_file >> /dev/null
-    if [ $? -ne 0 ]; then
-        echo $rc_file
+    if [ -e $HOME/$rc_file ]; 
+    then
+        diff $rc_file $HOME/$rc_file >> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "$rc_file has been changes."
+        fi
+    else
+        echo "$rc_file does not exists."
     fi
+
 done
 
