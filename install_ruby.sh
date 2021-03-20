fb3bd36b25ee4ffe97121b15f33bd16c6bfe4a4e
tieli <tiejli@yahoo.com>
2021-03-20 17:30:21 +0000

    update install_ruby.sh and install_rvm.sh to use github rawfile function

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
-
 source $HOME/.rvm/scripts/rvm
 
+display_usage() {
+    echo -e "\n==========================================\n"
+    echo -e "Usage: install_ruby ruby 2.7.1 rails 5.2\n"
+    echo -e "Usage: install_ruby ruby 3.0 rails 6.0\n"
+    echo -e "\n==========================================\n"
+}
+
+if [ $# -le 1 ]
+then
+    display_usage
+    exit 1
+fi
+
+if [[ ( $# == "--help") || $# == "-h" ]]
+then
+    display_usage
+    exit 0
+fi
+
+# shift out keyword ruby
+shift
 rvm use --default --install $1
 
 shift
-
 name=$1
 
+# shift out keyword rails
 shift
 
 while [ $# -gt 0 ]
--- install_rvm.sh
+++ install_rvm.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#curl -sSL https://get.rvm.io | bash -s $1
+
+###########################################
+# To fix 
+# Failed to connect to raw.githubusercontent.com port 443: Connection refused
+# Add the following line to /etc/hosts
+# 199.232.28.133 raw.githubusercontent.com
+###########################################
+
+################################
+# Sat Mar 20 09:39:21 UTC 2021
+################################
+
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
 curl -sSL https://get.rvm.io | bash -s $1
 
